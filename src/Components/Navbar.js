35a74990f00d3fb8bea35d1a8c5629fa9bfb2b02
UtkarshAgrawal578 <utkarshzhcet1234@gmail.com>
2025-08-17 00:26:02 -0700

    navbar change

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,53 +7,82 @@ const Navbar=(props)=>  {
 
 
     return (
-      <div>
-        <nav className={`navbar navbar-expand-lg fixed-top navbar-${props.back1} bg-${props.back1}`}>
-  <Link className="navbar-brand" to="/">NewsHub</Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon">
-      
-    </span>
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item">
-        <Link className="nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
-      </li>
-   
-       
-       <li className="nav-item"> <Link className="nav-link" to="/business">Buisness</Link></li> 
-       <li className="nav-item"> <Link className="nav-link" to="/entertainment">Entertainment</Link></li> 
-        <li className="nav-item"> <Link className="nav-link" to="/general">General</Link></li> 
-        <li className="nav-item"> <Link className="nav-link" to="/health">Health</Link></li> 
-        <li className="nav-item"> <Link className="nav-link" to="/science">Science</Link></li> 
-        <li className="nav-item"> <Link className="nav-link" to="/sports">Sports</Link></li> 
-        <li className="nav-item"> <Link className="nav-link" to="/technology">Technology</Link></li> 
-        
-    </ul>
-  </button>
-                                           
-  <div className="collapse navbar-collapse"  id="navbarSupportedContent">
-    
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item">
-        <Link className="nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
-      </li>
-   
-       
-       <li className="nav-item"> <Link className="nav-link" to="/business">Buisness</Link></li> 
-       <li className="nav-item"> <Link className="nav-link" to="/entertainment">Entertainment</Link></li> 
-        <li className="nav-item"> <Link className="nav-link" to="/general">General</Link></li> 
-        <li className="nav-item"> <Link className="nav-link" to="/health">Health</Link></li> 
-        <li className="nav-item"> <Link className="nav-link" to="/science">Science</Link></li> 
-        <li className="nav-item"> <Link className="nav-link" to="/sports">Sports</Link></li> 
-        <li className="nav-item"> <Link className="nav-link" to="/technology">Technology</Link></li> 
-        
-    </ul>
-      <button type="button"  onClick={props.togglemode1}  class="btn btn-outline-dark bg-success text-danger">{props.text1}</button>
-     
-    
-  </div>
-</nav>
+       <nav className={`navbar navbar-expand-lg fixed-top navbar-${props.back1} bg-${props.back1}`}>
+      <div className="container-fluid">
+        {/* Brand */}
+        <Link className="navbar-brand fw-bold" to="/">
+          NewsHub
+        </Link>
+
+        {/* Toggler button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navbar links */}
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/business">
+                Business
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/entertainment">
+                Entertainment
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/general">
+                General
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/health">
+                Health
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/science">
+                Science
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/sports">
+                Sports
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/technology">
+                Technology
+              </Link>
+            </li>
+          </ul>
+
+          {/* Toggle button for dark/light */}
+          {/* <button
+            type="button"
+            onClick={props.togglemode1}
+            className="btn btn-outline-dark bg-success text-danger"
+          >
+            {props.text1}
+          </button> */}
+        </div>
       </div>
+    </nav>
     )
 }
 export default Navbar
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,130 +1,118 @@
-import React, { useEffect,useState } from 'react'
-import NewsItem from './NewsItem'
-import Spinner from './Spinner'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
-const News =(props)=> {
-  
+
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [totalResults, setTotalResults] = useState(0);
+
+  // ✅ Default props
   News.defaultProps = {
     country: "us",
     pageSize: 8,
     category: "general",
-  }
+  };
+
   News.propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string,
-  }
-  const [articles,setArticles]=useState([]);
-  const [page,setPage]=useState(1);
-  const [loading,setLoading]=useState(true);
-  const [totalResults,setTotalResults]=useState(0);
-  
+    apiKey: PropTypes.string.isRequired,
+  };
 
-
- const updateNews= async ()=> {
+  // ✅ Fetch news (calls backend API instead of NewsAPI directly)
+  const updateNews = async () => {
     props.setProgress(10);
-    const url = `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-    setLoading(true)
-    let data = await fetch(url);
-    props.setProgress(30);
-    let parsedData = await data.json();
-    props.setProgress(70);
-    // console.log(parsedData)
-    setArticles(parsedData.articles)
-    setTotalResults(parsedData.totalResults)
-    // setPage(page+1)
-    setLoading(false)
-    
-   
-    props.setProgress(100);
-  }
-  useEffect(()=>{
+
+    try {
+      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+      setLoading(true);
+
+      let data = await fetch(url);
+      props.setProgress(40);
+
+      let parsedData = await data.json();
+      props.setProgress(70);
+
+      setArticles(parsedData.articles || []);
+      setTotalResults(parsedData.totalResults || 0);
+
+      setLoading(false);
+      props.setProgress(100);
+    } catch (error) {
+      console.error("Error fetching news:", error);
+      setLoading(false);
+      props.setProgress(100);
+    }
+  };
+
+  useEffect(() => {
     updateNews();
-  },[])
- 
-  // async updateNews(){
-  //    const url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=048263337dc049c99e94872b2fae4c55&page=${this.state.page}&pageSize=${props.pageSize}`;
-  //   this.setState({loading:true})
-  //    let data=await fetch(url);
-  //    let parsedData=await data.json();
-  //    console.log(parsedData)
-  //    this.setState({articles: parsedData.articles,
-  //                   page:this.state.page-1,
-  //                   loading:false
-  //    })
-  //  }
-    
+    // eslint-disable-next-line
+  }, [props.category]);
+
+  // ✅ Infinite Scroll handler
   const fetchMoreData = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
-    setPage(page+1)
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    setArticles(articles.concat(parsedData.articles))
-    setTotalResults(parsedData.totalResults)
-    setLoading(false)
-};
-    
-   
-  // handlenextclick=async ()=>{
-  //   // if(!(this.state.page+1>Math.ceil(this.state.totalResults/props.pageSize))){
-  //   //   let url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=048263337dc049c99e94872b2fae4c55&page=${this.state.page+1}&pageSize=${props.pageSize}`;
-  //   //   this.setState({loading:true})
-  //   //  let data=await fetch(url);
-  //   //  let parsedData=await data.json();
+    try {
+      const nextPage = page + 1;
+      const url = `https:///newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+      
+      let data = await fetch(url);
+      let parsedData = await data.json();
 
-  //   //  this.setState({articles: parsedData.articles,
-  //   //                 page:this.state.page+1,
-  //   //                 loading:false
-  //   //  })
-  //   // }
-  //   this.setState({page:this.state.page+1});
-  //   this.updateNews();
-  // }
-  // handleprevclick=async ()=>{
-  //   // let url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=048263337dc049c99e94872b2fae4c55&page=${this.state.page-1}&pageSize=${props.pageSize}`;
-  //   // this.setState({loading:true})
-  //   //  let data=await fetch(url);
-  //   //  let parsedData=await data.json();
-  //   //  console.log(parsedData)
-  //   //  this.setState({articles: parsedData.articles,
-  //   //                 page:this.state.page-1,
-  //   //                 loading:false
-  //   //  })
-  //   this.setState({page:this.state.page-1});
-  //   this.updateNews();
-  //}
-  
-    return (
-      <>
-        <h1 className='text-danger' style={{marginTop:'6rem',marginBottom:'2rem'}}><u><center>NewsHub-Top {props.category.toUpperCase()} Headlines </center></u></h1>
-        {loading && <Spinner />}
-        
-        <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length!==totalResults}
-          loader={articles.length!==totalResults?<Spinner/>:null}>
-           
-          {/* loader={<Spinner />} */}
-          
-          {/* // loader!=={<Spinner/>} */}
-          <div className="container">
-            <div className="row">
-              {articles.map((element) => {
-                return <div className="col-12 col-md-4 my-2" key={element.url}>
-                  <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name.slice(0, 17)} />
-                </div>
+      setArticles(articles.concat(parsedData.articles || []));
+      setTotalResults(parsedData.totalResults || 0);
+      setPage(nextPage);
+    } catch (error) {
+      console.error("Error fetching more news:", error);
+    }
+  };
 
-              })}
+  return (
+    <>
+      <h1
+        className="text-danger"
+        style={{ marginTop: "6rem", marginBottom: "2rem" }}
+      >
+        <u>
+          <center>
+            NewsHub - Top {props.category.toUpperCase()} Headlines
+          </center>
+        </u>
+      </h1>
 
-            </div>
+      {loading && <Spinner />}
+
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={articles.length !== totalResults ? <Spinner /> : null}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element) => (
+              <div className="col-12 col-md-4 my-2" key={element.url}>
+                <NewsItem
+                  title={element.title || ""}
+                  description={element.description}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source?.name?.slice(0, 17)}
+                />
+              </div>
+            ))}
           </div>
-        </InfiniteScroll>
-
-      </>
-    )
-  }
-
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+};
 
 export default News;
